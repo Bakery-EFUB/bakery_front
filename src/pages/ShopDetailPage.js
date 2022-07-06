df53b9d460cb7e130a447713eb26233a0315843b
o3o0903@gmail.com <o3o0903@gmail.com>
2022-07-07 01:11:42 +0900

    feat: init ShopDetailPage.js

---
+++ src/pages/ShopDetailPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+import TopBar from "../components/TopBar";
+
+const ShopDetailPage = () => {
+    return (
+        <div>
+            <TopBar/>
+        </div>
+    );
+};
+
+export default ShopDetailPage;
