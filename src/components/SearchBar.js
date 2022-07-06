fd57597b6135fb26afa5915b133b0abb21920ca4
harloxx <chloe0105@naver.com>
2022-07-07 08:42:37 +0900

    feat:searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,11 +5,38 @@ import SearchIcon from "../images/SearchIcon.svg";
 const SearchBar = ({ text }) => {
   const SearchBarContainer = styled.div`
     border-radius: 6px;
+    position: relative;
+    width: 100%;
+    height: 50px;
     background-color: var(--sub-lightpink);
   `;
+
+  const Content = styled.div`
+    display: flex;
+    align-items: center;
+    margin: 20px;
+    p {
+      color: var(--main-pink);
+      font-weight: 500;
+      font-family: "AppleSDGothicNeo";
+    }
+  `;
+
+  const SearchLogo = styled.div`
+    background: url(${SearchIcon});
+    width: 21.29px;
+    height: 21.29px;
+    margin-right: 15px;
+  `;
+
   return (
     <div>
-      <SearchBarContainer>{text}</SearchBarContainer>
+      <SearchBarContainer>
+        <Content>
+          <SearchLogo></SearchLogo>
+          <p>{text}</p>
+        </Content>
+      </SearchBarContainer>
     </div>
   );
 };
--- src/pages/MainHome.js
+++ src/pages/MainHome.js
@@ -1,12 +1,18 @@
 import React from "react";
 import SearchBar from "../components/SearchBar";
 import TopBar from "../components/TopBar";
+import styled from "styled-components";
 
 const MainHome = () => {
+  const HomeContainer = styled.div`
+    margin: 0 5%;
+  `;
   return (
     <div>
       <TopBar></TopBar>
-      <SearchBar text={"케이크, 가게 검색"} />
+      <HomeContainer>
+        <SearchBar text={"케이크, 가게 검색"} />
+      </HomeContainer>
     </div>
   );
 };
