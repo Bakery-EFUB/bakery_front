b1a5563a9f4852061675cc81af6761706f74513a
JiwonKim08 <99666136+JiwonKim08@users.noreply.github.com>
2022-07-06 14:00:36 +0900

    feat:css적용, styled적용 이전

--- src/pages/ShopMemberMyPage.js
+++ src/pages/ShopMemberMyPage.js
@@ -1,6 +1,15 @@
 import "./ShopMemberMyPage.css";
 import TopBar from "../components/TopBar";
+import styled from "styled-components";
 
+const Box = styled.div`
+  background-color: #ffebea;
+  position: absolute;
+  width: 428px;
+  height: 619.09px;
+  left: 0px;
+  top: 306.91px;
+`;
 const ShopMemberMyPage = () => {
   return (
     <div>
@@ -10,7 +19,7 @@ const ShopMemberMyPage = () => {
         <div>닉네임 님,</div>
         <div>이미지 삽입</div>
       </div>
-      <section className="BottomPinkBackground">
+      <Box className="BottomPinkBackground">
         <div className="WriteProposal">제안서 작성하기</div>
         <div className="BottomProposal">
           <article>1</article>
@@ -21,7 +30,7 @@ const ShopMemberMyPage = () => {
           <article>6</article>
         </div>
         <div>이미지 삽입</div>
-      </section>
+      </Box>
     </div>
   );
 };
