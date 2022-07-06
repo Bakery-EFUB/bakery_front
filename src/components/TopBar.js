6790474e5c0f8cec340bb31e705af2dd028e54ae
JiwonKim08 <99666136+JiwonKim08@users.noreply.github.com>
2022-07-06 13:12:33 +0900

    feat:상단바 컴포넌트 추가

---
+++ src/components/TopBar.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import "./TopBar.css";
+
+const TopBar = () => {
+  return (
+    <div>
+      {" "}
+      <div className="TopBar">
+        <div className="TopBox">
+          {" "}
+          <div className="TopLogo"></div>
+          <div className="TopMenuBar"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopBar;
