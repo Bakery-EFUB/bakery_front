197065642fea7ce181559b6b02f473672f2864c5
JiwonKim08 <99666136+JiwonKim08@users.noreply.github.com>
2022-07-07 13:29:52 +0900

    feat: eslint오류 다 해결

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-<<<<<<< HEAD
 import "./App.css";
-import TopBar from "./components/TopBar";
-import ShopMemberMyPage from "./pages/ShopMemberMyPage";
-import ShopMyPageProgressive from "./pages/ShopMyPageProgressive";
-import ShopMyPageStart from "./pages/ShopMyPageStart";
-=======
-import './App.css';
-import Proposal6 from './pages/Proposal6';
->>>>>>> ad0c1986eedb81f00de8d2fa00334b738a73e7d7
 
-import ShopInformationRegister from "./pages/ShopInformationRegister";
-import Uploading from "./components/Uploading";
+import ShopMemberMyPage from "./pages/ShopMemberMyPage";
+
 function App() {
-<<<<<<< HEAD
   return (
     <div className="App">
-      <ShopInformationRegister />
+      <ShopMemberMyPage />
     </div>
   );
-=======
-	return <Proposal6 />;
->>>>>>> ad0c1986eedb81f00de8d2fa00334b738a73e7d7
 }
 
 export default App;
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import styled from "styled-components";
 import TopPinkBoximg from "../images/TopPinkBox.svg";
 import TopLogoimg from "../images/TopLogo.svg";
@@ -47,59 +46,6 @@ const TopBar = () => {
       </TopBarPink>
     </div>
   );
-=======
-import styled from 'styled-components';
-import TopLogoimg from '../images/TopLogo.svg';
-import TopMenuBarimg from '../images/TopMenuBar.svg';
-
-import '../styles/common.scss';
-
-const TopBarPink = styled.div`
-	background-color: var(--main-pink);
-
-	width: 100%;
-	overflow: hidden;
-	height: 60px;
-
-	position: absolute;
-	left: 0%;
-	right: 0%;
-	top: 0%;
-	bottom: 0%;
-`;
-
-const TopLogo = styled.div`
-	background: url(${TopLogoimg});
-	width: 130.13px;
-	height: 33px;
-	position: absolute;
-	left: 34.79%;
-	right: 34.8%;
-	top: 21.71%;
-	bottom: 23.29%;
-`;
-
-const TopMenuBar = styled.div`
-	background: url(${TopMenuBarimg});
-	width: 20px;
-	height: 21px;
-	position: absolute;
-	left: 5.61%;
-	right: 89.72%;
-	top: 35%;
-	bottom: 36.59%;
-`;
-
-const TopBar = () => {
-	return (
-		<div>
-			<TopBarPink>
-				<TopLogo></TopLogo>
-				<TopMenuBar></TopMenuBar>
-			</TopBarPink>
-		</div>
-	);
->>>>>>> ad0c1986eedb81f00de8d2fa00334b738a73e7d7
 };
 
 export default TopBar;
