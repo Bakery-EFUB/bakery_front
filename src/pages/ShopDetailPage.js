cd8d539c88a6c9f193789dab7fb7e378a4a79667
o3o0903@gmail.com <o3o0903@gmail.com>
2022-07-07 21:33:28 +0900

    feat : add page title

--- src/pages/ShopDetailPage.js
+++ src/pages/ShopDetailPage.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
 import TopBar from "../components/TopBar";
+import PageTitle from "../components/PageTitle";
 
 const ShopDetailPage = () => {
-    return (
-        <div>
-            <TopBar/>
-        </div>
-    );
+  return (
+    <div>
+      <TopBar />
+      <PageTitle title="가게 상세정보" />
+    </div>
+  );
 };
 
 export default ShopDetailPage;
