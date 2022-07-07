d0773c8254b87d6ecd4b2e928cf86cadf713e801
dayun <81161750+dy6578ekdbs@users.noreply.github.com>
2022-07-07 16:19:47 +0900

    feat : creat Proposal 7

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import Proposal6 from './pages/Proposal6';
-
+import Proposal7 from './pages/Proposal7';
+import ProposalDone from './pages/ProposalDons';
 function App() {
-	return <Proposal6 />;
+	return <Proposal7 />;
 }
 
 export default App;
