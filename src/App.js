ce318f04a62274008554f20f136bf65d6aeb8947
JiwonKim08 <99666136+JiwonKim08@users.noreply.github.com>
2022-07-07 12:53:19 +0900

    feat:페이지 완성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import TopBar from "./components/TopBar";
 import ShopMemberMyPage from "./pages/ShopMemberMyPage";
+import ShopMyPageProgressive from "./pages/ShopMyPageProgressive";
+import ShopMyPageStart from "./pages/ShopMyPageStart";
 
+import ShopInformationRegister from "./pages/ShopInformationRegister";
+import Uploading from "./components/Uploading";
 function App() {
   return (
     <div className="App">
-      <ShopMemberMyPage></ShopMemberMyPage>
+      <ShopInformationRegister />
     </div>
   );
 }
