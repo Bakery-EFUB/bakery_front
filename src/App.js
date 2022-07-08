cba989af42de825fd918832abd4482beef4b1a37
dayun <81161750+dy6578ekdbs@users.noreply.github.com>
2022-07-08 18:19:17 +0900

    feat : ProposalDone 완성

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import "./App.css";
-import Proposal6 from "./pages/Proposal6";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoadingPage from "./pages/LoadingPage";
-import LoginHome from "./pages/LoginHome";
-import MainHome from "./pages/MainHome";
+import './App.css';
+import Proposal6 from './pages/Proposal6';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoadingPage from './pages/LoadingPage';
+import LoginHome from './pages/LoginHome';
+import MainHome from './pages/MainHome';
+import ProposalDone from './pages/ProposalDone';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/loginhome" element={<LoginHome />} />
-        <Route exact path="/loading" element={<LoadingPage />} />
-        <Route exact path="/proposal" element={<Proposal6 />} />
-        <Route exact path="/home" element={<MainHome />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route exact path="/loginhome" element={<LoginHome />} />
+				<Route exact path="/loading" element={<LoadingPage />} />
+				<Route exact path="/proposal" element={<ProposalDone />} />
+				<Route exact path="/home" element={<MainHome />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
