f69550a07de57e54b3bc2700f26d64383fe0ee9e
dayun <81161750+dy6578ekdbs@users.noreply.github.com>
2022-07-09 16:32:45 +0900

    feat : 제목 추가 & 컴포넌트 위치 조정

--- src/pages/Proposal7.js
+++ src/pages/Proposal7.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import TopBar from '../components/TopBar';
 
 import SmallWhiteButton from '../components/SmallWhiteButton';
 import SmallGrayButton from '../components/SmallGrayButton';
+import SmallPinkButton from '../components/SmallPinkButton';
 
 import CustomCalendar from '../components/CustomCalendar';
+import PageTitle from '../components/PageTitle';
 
-const Text2 = styled.p`
+const ProgessBar = styled.div`
+	width: 380px;
+	height: 4px;
+
+	margin: 11px auto 0 auto;
+
+	background-color: var(--main-pink);
+	border: none;
+`;
+
+const Text = styled.p`
 	font-family: 'Apple SD Gothic Neo';
 
 	font-style: normal;
@@ -16,80 +28,63 @@ const Text2 = styled.p`
 	line-height: 26px;
 	text-align: center;
 
-	position: absolute;
 	width: 290px;
 	height: 26px;
-	left: 68px;
-	top: 264px;
 
-	margin: 0;
-`;
-
-const ProgessBar = styled.div`
-	width: 380px;
-	height: 4px;
-
-	position: absolute;
-
-	left: 24px;
-	top: 203px;
-
-	background-color: var(--main-pink);
-	border: none;
-`;
-
-const Button = styled.button`
-	margin-top: 20px;
-
-	position: absolute;
-	width: 380px;
-	height: 60px;
-	left: 24px;
-	top: 310px;
-
-	background-color: var(--main-pink);
-	border-radius: 6px;
-	box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-
-	border: none;
-
-	color: var(--sub-yellow);
-
-	font-family: 'Apple SD Gothic Neo';
-	font-style: normal;
-	font-weight: 500;
-	font-size: 18px;
-	line-height: 22px;
-	/* identical to box height */
-	text-align: center;
-	text-transform: uppercase;
+	margin: 57px auto 0 auto;
 `;
 
 const ImageBox = styled.div`
-	margin-top: 19px;
-	position: absolute;
+	margin-top: 27px;
+
 	width: 380px;
 	height: 256px;
-	left: 24px;
-	top: 389px;
-
-	/* background */
 
 	background: var(--background);
 	box-shadow: 0px 4px 62px rgba(153, 171, 198, 0.18);
 	border-radius: 6px;
 `;
 
+const CalendarSection = styled.div`
+	position: 'relative';
+	margin-top: 27px;
+
+	width: 100%;
+	height: 424px;
+`;
+
 const Proposal7 = () => {
+	const [isDone, setIsDone] = useState(false);
 	return (
 		<div>
 			<TopBar />
-			{/* <Text1>제안서 작성하기</Text1> */}
+			<PageTitle margin="56px auto 0 auto" title="제안서 작성하기" />
+
 			<ProgessBar />
-			<Text2>픽업 날짜를 선택해주세요.</Text2>
-			<CustomCalendar />
-			<SmallWhiteButton>이전</SmallWhiteButton>
-			<SmallGrayButton>완료</SmallGrayButton>
+			<Text>픽업 날짜를 선택해주세요.</Text>
+			<CalendarSection>
+				<CustomCalendar />
+			</CalendarSection>
+
+			<div
+				style={{
+					margin: '105px 24px auto 24px',
+					display: 'flex',
+					justifyContent: 'space-between',
+				}}
+			>
+				<SmallWhiteButton>이전</SmallWhiteButton>
+
+				{isDone ? (
+					<SmallPinkButton onClick={() => setIsDone(!isDone)}>
+						완료
+					</SmallPinkButton>
+				) : (
+					<SmallGrayButton onClick={() => setIsDone(!isDone)}>
+						완료
+					</SmallGrayButton>
+				)}
+			</div>
 		</div>
 	);
 };
