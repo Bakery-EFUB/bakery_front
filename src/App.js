b5bb9abce8ea1149fbb97be2057348be5a973753
sohyundoh <lyny123@naver.com>
2022-07-09 22:01:35 +0200

    feat: 전체 제안서 보기 제안서 컴포넌트 수정

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import "./App.css";
-import Proposal6 from "./pages/Proposal6";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoadingPage from "./pages/LoadingPage";
-import LoginHome from "./pages/LoginHome";
-import MainHome from "./pages/MainHome";
+import ProposalBox from "./components/WholeProposals/PropasalBox";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/loginhome" element={<LoginHome />} />
-        <Route exact path="/loading" element={<LoadingPage />} />
-        <Route exact path="/proposal" element={<Proposal6 />} />
-        <Route exact path="/home" element={<MainHome />} />
-      </Routes>
-    </BrowserRouter>
+  <ProposalBox/>
   );
 }
 
---
+++ src/components/WholeProposals/PropasalBox.js
@@ -0,0 +1,60 @@
+import React from "react";
+import styled from "styled-components";
+import Rectangle from '../../images/Rectangle.svg';
+const Oneproposal = styled.div`
+`;
+const Cakeimg = styled.div`
+    border-radius: 15%;
+    margin : 15px;
+`;
+const Textbox = styled.div`
+    border-left: 15px;
+    flex-direction: row;
+    width: 250px;
+    height: 100px;
+`;
+const Title = styled.div`
+    color: black;
+    font-weight: bold;
+    font-size: 20px;
+    margin : 5px;  
+`;
+const Description = styled.div`
+    color: black;
+    margin : 5px;
+    font-size: 15px;
+`;
+const Hashtag = styled.div`
+    color: #FF7B72;
+    font-size: 10px;
+    margin-left: 10px;
+`;
+const HashtagBox = styled.div`
+    margin-top: 100px;
+    flex-direction: column;
+`;
+const Timeshow = styled.div`
+    color: #949494;
+    font-size: 10px;
+`;
+const ProposalBox = ()=>{
+    return(
+        <Oneproposal>
+            <Cakeimg>
+                <img src={Rectangle}/>
+            </Cakeimg>
+            <Textbox>
+                <Title>cake1235</Title>
+                <Description>레터링케이크</Description>
+                <HashtagBox>
+                    <Hashtag>#서대문구</Hashtag>
+                    <Hashtag>#북아현동</Hashtag>
+                </HashtagBox>
+            </Textbox>
+            <Timeshow>
+                10분전
+            </Timeshow>
+        </Oneproposal>
+    )
+};
+export default ProposalBox;
