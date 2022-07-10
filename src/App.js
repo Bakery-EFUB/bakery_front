2a2ebf1cd5db54e00e5617ec31346e3ef4c51e8f
sohyundoh <lyny123@naver.com>
2022-07-10 06:23:26 +0200

    feat : 햄버거바 수정

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Proposal6 from './pages/Proposal6';
 
 function App() {
-	return <Proposal6 />;
 }
 
 export default App;
---
+++ src/pages/Hamburger.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const ContentWrap = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+const ProfileWrap = styled.div`
+    flex-direction: row;
+
+`;
+const ProfileText = styled.div`
+    flex-direction: column;
+`;
+const Hamburger = () =>{
+
+}
+export default Hamburger;
