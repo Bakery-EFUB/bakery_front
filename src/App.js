32e43b0a28ce18ab219de84b9e4081f46b6abaa9
sohyundoh <lyny123@naver.com>
2022-07-10 05:42:27 +0200

    feat: ProposalPage  수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import ProposalPage from "./pages/ProposalPage";
+import TopBar from "./components/TopBar";
 import ProposalBox from "./components/WholeProposals/PropasalBox";
 import ChooseBox from "./components/WholeProposals/ChooseBox";
+import ProposalTitle from "./images/ProposalTitle.svg"
 function App() {
   return (
     <div>
+      <TopBar/>
+      <img src={ProposalTitle}/>
       <ChooseBox/>
       <ProposalBox/>
     </div>
--- src/pages/ProposalPage.js
+++ src/pages/ProposalPage.js
@@ -4,18 +4,16 @@ import TopBar from "../components/TopBar";
 import ProposalBox from "../components/WholeProposals/PropasalBox";
 import ProposalTitle from "../images/ProposalTitle.svg";
 import ChooseBox from "../components/WholeProposals/ChooseBox";
-const PageView = styled.div`
-    align-items: center;
-`;
+
 const TextProposal = styled.div`
     margin-top: 100px;
 `;
 const ProposalBoxSet = styled.div`
+    margin-top: 50px;
 `;
 const ProposalPage = ()=>{
-    <PageView>
-        <TopBar>
-       </TopBar>
+    <div>
+        <TopBar/>
         <TextProposal>
             <img src={ProposalTitle}/>
         </TextProposal>
@@ -23,6 +21,6 @@ const ProposalPage = ()=>{
         <ProposalBoxSet>
             <ProposalBox></ProposalBox>
         </ProposalBoxSet>
-    </PageView>
+    </div>
 };
 export default ProposalPage;
