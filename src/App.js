1edb18ffc475f6b79827ac69cfac9bd46e269b80
harloxx <chloe0105@naver.com>
2022-07-12 07:31:54 +0900

    feat: 검색 결과 페이지

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import './App.css';
-import Proposal6 from './pages/Proposal6';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoadingPage from './pages/LoadingPage';
-import LoginHome from './pages/LoginHome';
-import MainHome from './pages/MainHome';
-import MyProposal from './pages/MyProposal';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoadingPage from "./pages/LoadingPage";
+import LoginHome from "./pages/LoginHome";
+import MainHome from "./pages/MainHome";
+import MyProposal from "./pages/MyProposal";
+import SearchPage from "./pages/SearchPage";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route exact path="/loginhome" element={<LoginHome />} />
-				<Route exact path="/loading" element={<LoadingPage />} />
-				<Route exact path="/proposal" element={<MyProposal />} />
-				<Route exact path="/home" element={<MainHome />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/loginhome" element={<LoginHome />} />
+        <Route exact path="/loading" element={<LoadingPage />} />
+        <Route exact path="/proposal" element={<MyProposal />} />
+        <Route exact path="/home" element={<MainHome />} />
+        <Route exact path="/search" element={<SearchPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/SearchResult/SearchResult.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+import Search from "../../images/SearchResult.svg";
+import BigCardDisplay from "../BigCardDisplay";
+
+const SearchResult = () => {
+  return (
+    <>
+      <BigCardDisplay MainText="달다구리" SubText="미등록"></BigCardDisplay>
+    </>
+  );
+};
+
+export default SearchResult;
