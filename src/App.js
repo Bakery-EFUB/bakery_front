d795e4dbe379d257d71daad1d4ac053f097d06f8
dayun <81161750+dy6578ekdbs@users.noreply.github.com>
2022-07-12 22:25:37 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ import MainHome from "./pages/MainHome";
 import ProposalSize from "./pages/ProposalSize";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route exact path="/loginhome" element={<LoginHome />} />
-				<Route exact path="/loading" element={<LoadingPage />} />
-				<Route exact path="/proposal" element={<Proposal6 />} />
-				<Route exact path="/home" element={<MainHome />} />
-			</Routes>
-		</BrowserRouter>
-	);
+ return (
+  <BrowserRouter>
+   <Routes>
+    <Route exact path="/loginhome" element={<LoginHome />} />
+    <Route exact path="/loading" element={<LoadingPage />} />
+    <Route exact path="/proposal" element={<Proposal6 />} />
+    <Route exact path="/home" element={<MainHome />} />
+   </Routes>
+  </BrowserRouter>
+ );
 }
 
 export default App;
