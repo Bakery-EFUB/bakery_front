086efd6b81f122e009087107ac921a097b151efd
dayun <81161750+dy6578ekdbs@users.noreply.github.com>
2022-07-12 22:33:08 +0900

    fix : indent setting

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import './App.css';
-import Proposal6 from './pages/Proposal6';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoadingPage from './pages/LoadingPage';
-import LoginHome from './pages/LoginHome';
-import MainHome from './pages/MainHome';
-import ProposalCake from './pages/ProposalCake';
+import "./App.css";
+import Proposal6 from "./pages/Proposal6";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoadingPage from "./pages/LoadingPage";
+import LoginHome from "./pages/LoginHome";
+import MainHome from "./pages/MainHome";
+import ProposalCake from "./pages/ProposalCake";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route exact path="/loginhome" element={<LoginHome />} />
-				<Route exact path="/loading" element={<LoadingPage />} />
-				<Route exact path="/proposal" element={<ProposalCake />} />
-				<Route exact path="/home" element={<MainHome />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/loginhome" element={<LoginHome />} />
+        <Route exact path="/loading" element={<LoadingPage />} />
+        <Route exact path="/proposal" element={<ProposalCake />} />
+        <Route exact path="/home" element={<MainHome />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
