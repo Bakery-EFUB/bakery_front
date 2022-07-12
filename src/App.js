12090975fcf2bc2f8b60e4f9ccc042fbda098274
dayun <81161750+dy6578ekdbs@users.noreply.github.com>
2022-07-12 10:28:31 +0900

    feat : Proposal Cake

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoadingPage from './pages/LoadingPage';
 import LoginHome from './pages/LoginHome';
 import MainHome from './pages/MainHome';
-import MyProposal from './pages/MyProposal';
+import ProposalCake from './pages/ProposalCake';
 
 function App() {
 	return (
@@ -12,7 +12,7 @@ function App() {
 			<Routes>
 				<Route exact path="/loginhome" element={<LoginHome />} />
 				<Route exact path="/loading" element={<LoadingPage />} />
-				<Route exact path="/proposal" element={<MyProposal />} />
+				<Route exact path="/proposal" element={<ProposalCake />} />
 				<Route exact path="/home" element={<MainHome />} />
 			</Routes>
 		</BrowserRouter>
