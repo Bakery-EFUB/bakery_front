1561aa083dcd34a14f785466af76d8006766df58
JiwonKim08 <99666136+JiwonKim08@users.noreply.github.com>
2022-07-13 20:03:15 +0900

    feat: ui 공백생김

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import './App.css';
-import Proposal6 from './pages/Proposal6';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoadingPage from './pages/LoadingPage';
-import LoginHome from './pages/LoginHome';
-import MainHome from './pages/MainHome';
-import MyProposal from './pages/MyProposal';
+import "./App.css";
+import Proposal6 from "./pages/Proposal6";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoadingPage from "./pages/LoadingPage";
+import LoginHome from "./pages/LoginHome";
+import MainHome from "./pages/MainHome";
+import MyProposal from "./pages/MyProposal";
+
+import ShopMemberMyPage from "./pages/ShopMemberMyPage";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route exact path="/loginhome" element={<LoginHome />} />
-				<Route exact path="/loading" element={<LoadingPage />} />
-				<Route exact path="/proposal" element={<MyProposal />} />
-				<Route exact path="/home" element={<MainHome />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/loginhome" element={<LoginHome />} />
+        <Route exact path="/loading" element={<LoadingPage />} />
+        <Route exact path="/proposal" element={<MyProposal />} />
+        <Route exact path="/home" element={<MainHome />} />
+        <Route exact path="/h" element={<ShopMemberMyPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
