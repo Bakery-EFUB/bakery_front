aad691360a8ca3497cbae0e954f4a6d3e89591e2
dayun <81161750+dy6578ekdbs@users.noreply.github.com>
2022-07-13 16:49:36 +0900

    fix : font-family 누락 수정

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -1,37 +1,38 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const PageTitle = ({ title, margin }) => {
-	const PageTitleBox = styled.div`
-		display: flex;
-		justify-content: center;
+  const PageTitleBox = styled.div`
+    display: flex;
+    justify-content: center;
 
-		margin: ${margin};
-	`;
+    margin: ${margin};
+  `;
 
-	const Highlight = styled.div`
-		display: inline-block;
+  const Highlight = styled.div`
+    display: inline-block;
 
-		height: 13px;
-		background-color: #ffebea;
-		vertical-align: center;
-	`;
+    height: 13px;
+    background-color: #ffebea;
+    vertical-align: center;
+  `;
 
-	const TitleText = styled.div`
-		width: fit-content;
-		font-size: 22px;
-		font-weight: bold;
-		margin: 0 3px;
-		position: relative;
-		bottom: 9px;
-	`;
+  const TitleText = styled.div`
+    width: fit-content;
+    font-family: "Apple SD Gothic Neo";
+    font-size: 22px;
+    font-weight: bold;
+    margin: 0 3px;
+    position: relative;
+    bottom: 9px;
+  `;
 
-	return (
-		<PageTitleBox>
-			<Highlight>
-				<TitleText>{title}</TitleText>
-			</Highlight>
-		</PageTitleBox>
-	);
+  return (
+    <PageTitleBox>
+      <Highlight>
+        <TitleText>{title}</TitleText>
+      </Highlight>
+    </PageTitleBox>
+  );
 };
 
 export default PageTitle;
