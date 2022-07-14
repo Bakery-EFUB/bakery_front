71dec606f95ca362d7459b0e3e84fcd37917fafb
dayun <81161750+dy6578ekdbs@users.noreply.github.com>
2022-07-14 23:10:42 +0900

    feat : App.js 통합제안서 페이지 추가

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Route exact path="/loginhome" element={<LoginHome />} />
         <Route exact path="/loading" element={<LoadingPage />} />
 
-        <Route exact path="/proposal" element={<Proposal />} />
+        <Route path="/proposal/*" element={<Proposal />} />
 
         <Route exact path="/home" element={<MainHome />} />
       </Routes>
