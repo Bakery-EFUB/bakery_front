80b7a7f9b3c8b50af89a32165d1044156e272bb0
dayun <81161750+dy6578ekdbs@users.noreply.github.com>
2022-07-14 18:23:20 +0900

    feat : 제안서 통합 페이지 생성

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 
-import Proposal6 from "./pages/Proposal6";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoadingPage from "./pages/LoadingPage";
 import LoginHome from "./pages/LoginHome";
 import MainHome from "./pages/MainHome";
 
-import ProposalPrice from "./pages/Proposal/ProposalPrice";
+import Proposal from "./pages/Proposal";
 
 function App() {
   return (
@@ -16,7 +14,7 @@ function App() {
         <Route exact path="/loginhome" element={<LoginHome />} />
         <Route exact path="/loading" element={<LoadingPage />} />
 
-        <Route exact path="/proposal" element={<ProposalCake />} />
+        <Route exact path="/proposal" element={<Proposal />} />
 
         <Route exact path="/home" element={<MainHome />} />
       </Routes>
---
+++ src/pages/Proposal.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import TopBar from "../components/TopBar";
+
+import SmallWhiteButton from "../components/SmallWhiteButton";
+import SmallGrayButton from "../components/SmallGrayButton";
+import SmallPinkButton from "../components/SmallPinkButton";
+
+import PageTitle from "../components/PageTitle";
+
+const ProgessBar = styled.div`
+  height: 4px;
+
+  margin: 11px 24px 0 24px;
+
+  background-color: var(--main-pink);
+  border: none;
+`;
+
+const Text = styled.p`
+  font-family: "Apple SD Gothic Neo";
+
+  font-style: normal;
+  font-weight: 700;
+  font-size: 22px;
+  line-height: 26px;
+  text-align: center;
+
+  height: 26px;
+
+  margin: 57px auto 0 auto;
+`;
+
+const Proposal = () => {
+  const [isDone, setIsDone] = useState(false);
+
+  return (
+    <div>
+      <TopBar />
+      <PageTitle margin="56px auto 0 auto" title="제안서 작성하기" />
+
+      <ProgessBar />
+      <Text>픽업 날짜를 선택해주세요.</Text>
+
+      <div
+        style={{
+          width: "100%",
+          margin: "105px  auto 0 auto",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <SmallWhiteButton>이전</SmallWhiteButton>
+
+        <div style={{ marginLeft: "6px" }}>
+          {isDone ? (
+            <SmallPinkButton onClick={() => setIsDone(!isDone)}>
+              완료
+            </SmallPinkButton>
+          ) : (
+            <SmallGrayButton onClick={() => setIsDone(!isDone)}>
+              완료
+            </SmallGrayButton>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Proposal;
