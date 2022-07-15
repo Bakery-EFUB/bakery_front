d11275fcd69790494136565ffe5ac8f922a732f8
JiwonKim08 <99666136+JiwonKim08@users.noreply.github.com>
2022-07-15 13:26:39 +0900

    Feat: 오류 발생

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 
-import MyProposal from "./pages/MyProposal";
+import Proposal6 from "./pages/Proposal6";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoadingPage from "./pages/LoadingPage";
 import LoginHome from "./pages/LoginHome";
 import MainHome from "./pages/MainHome";
+
+import ProposalPrice from "./pages/Proposal/ProposalPrice";
 
 function App() {
   return (
@@ -13,7 +15,9 @@ function App() {
       <Routes>
         <Route exact path="/loginhome" element={<LoginHome />} />
         <Route exact path="/loading" element={<LoadingPage />} />
-        <Route exact path="/proposal" element={<MyProposal />} />
+
+        <Route exact path="/proposal" element={<ProposalCake />} />
+
         <Route exact path="/home" element={<MainHome />} />
       </Routes>
     </BrowserRouter>
--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -1,37 +1,46 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const PageTitle = ({ title, margin }) => {
-	const PageTitleBox = styled.div`
-		display: flex;
-		justify-content: center;
+  const PageTitleBox = styled.div`
+    display: flex;
+    justify-content: center;
 
-		margin: ${margin};
-	`;
+    margin: ${margin};
+  `;
 
-	const Highlight = styled.div`
-		display: inline-block;
+  const Highlight = styled.div`
+    display: inline-block;
 
-		height: 13px;
+  	height: 13px;
 		background-color: var(--sub-pink);
 		vertical-align: center;
-	`;
+  `;
+
 
 	const TitleText = styled.div`
 		width: fit-content;
-		font-size: 22px;
-		font-weight: bold;
+
 		margin: 0 3px;
 		position: relative;
 		bottom: 9px;
+
+		font-family: 'Apple SD Gothic Neo';
+		font-style: normal;
+		font-weight: 700;
+		font-size: 22px;
+		line-height: 26px;
+		text-align: center;
+
+		color: var(--black-text);
 	`;
 
-	return (
-		<PageTitleBox>
-			<Highlight>
-				<TitleText>{title}</TitleText>
-			</Highlight>
-		</PageTitleBox>
-	);
+  return (
+    <PageTitleBox>
+      <Highlight>
+        <TitleText>{title}</TitleText>
+      </Highlight>
+    </PageTitleBox>
+  );
 };
 
 export default PageTitle;
