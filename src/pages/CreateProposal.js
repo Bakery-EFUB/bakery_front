709d548bcc804d5907fb529a0bff302d32642b91
dayun <81161750+dy6578ekdbs@users.noreply.github.com>
2022-07-20 17:32:33 +0900

    feat : city 선택 저장 변수

--- src/pages/CreateProposal.js
+++ src/pages/CreateProposal.js
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { Route, Routes } from "react-router-dom";
 
 import TopBar from "../components/TopBar";
-
 import PickUp from "./CreateProposal/PickUp";
 import City from "./CreateProposal/City";
 import Cake from "./CreateProposal/Cake";
@@ -13,19 +12,52 @@ import Price from "./CreateProposal/Price";
 import Design from "./CreateProposal/Design";
 import Done from "./CreateProposal/Done";
 
+import AIP from "../components/API";
+
 const CreateProposal = () => {
   const [history, setHistory] = useState(0);
+  const [oriCity, setOriCity] = useState([3]);
+  const [oriCake, setOriCake] = useState(null);
+
+  useEffect(() => {
+    console.log("됏다", oriCity);
+  }, [oriCity]);
+
+  // const CreateProposal = () => {
+  //   const response = await API.post("/orders",{city:city, })
+  //   .then( (response) => {
+  //     console.log(response);
+  //   })
+  //   .catch( (error)=> {
+  //     console.log(error);
+  //   });
+  // }
+
   return (
     <div>
       <TopBar />
       <Routes>
         <Route
           path="/city"
-          element={<City history={history} setHistory={setHistory} />}
+          element={
+            <City
+              history={history}
+              setHistory={setHistory}
+              oriCity={oriCity}
+              setOriCity={setOriCity}
+            />
+          }
         />
         <Route
           path="/cake"
-          element={<Cake history={history} setHistory={setHistory} />}
+          element={
+            <Cake
+              history={history}
+              setHistory={setHistory}
+              oriCake={oriCake}
+              setOriCake={setOriCake}
+            />
+          }
         />
         <Route
           path="/size"
