9c854341a125cbec4aa5ef18608019d6733d7a2f
sohyundoh <lyny123@naver.com>
2022-07-31 19:15:01 +0200

    feat: UI 수정 및 api 연동

--- src/pages/AllProposal.js
+++ src/pages/AllProposal.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import TopBar from "../components/TopBar";
 import ChooseBox from "../components/WholeProposals/ChooseBox";
 import NoProposal from "../components/WholeProposals/NoProposal";
 import PageTitle from "../components/PageTitle";
-
+import { GetProposals } from "../api/allproposal";
+import ProposalBox from "../components/WholeProposals/PropasalBox";
+import styled from "styled-components";
+const ProposalsDisplay = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 const AllProposal = () => {
+  const [allProposals, setAllProposals] = useState([]);
+  const proposalApi = () => {
+    GetProposals()
+    .then(
+       data =>{
+        setAllProposals(data);
+      })
+    .catch(e => {
+      console.log(e);
+    });
+  };
+  useEffect(() =>{
+    proposalApi();
+  },[]);
   return (
     <div>
       <TopBar />
       <PageTitle title="전체 제안서 리스트" margin="60px 0 63px 0"></PageTitle>
       <ChooseBox></ChooseBox>
-      <NoProposal />
+      <ProposalsDisplay>
+        {allProposals.map(orders =>{
+          return (<ProposalBox key= {orders.member.nickname} description ={orders.type} time ={orders.createdAt} hashtag={hashtag}></ProposalBox>
+          );
+        })}
+      </ProposalsDisplay>
     </div>
   );
 };
