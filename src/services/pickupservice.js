d720b8e09498459971812925daae3c8f3c989563
o3o0903@gmail.com <o3o0903@gmail.com>
2022-08-04 10:46:15 +0900

    fix: api 변경 store -> stores

--- src/services/pickupservice.js
+++ src/services/pickupservice.js
@@ -1,9 +1,9 @@
 import http from "../common/http";
 
 const PickupService = {
-  getAllSchedules: storeId => http.get(`/store/${storeId}/events`),
+  getAllSchedules: storeId => http.get(`/stores/${storeId}/events`),
   deleteSchedule: (storeId, eventId) =>
-    http.delete(`/store/${storeId}/events/${eventId}`),
+    http.delete(`/stores/${storeId}/events/${eventId}`),
   addSchedule: data => http.post("/events", data),
 };
 export default PickupService;
