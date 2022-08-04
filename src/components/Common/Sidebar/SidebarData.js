18a03afd1efbc1a9ebd8cae910dc320602b23a27
dayun <81161750+dy6578ekdbs@users.noreply.github.com>
2022-08-04 21:52:05 +0900

    fix : path 수정

--- src/components/Common/Sidebar/SidebarData.js
+++ src/components/Common/Sidebar/SidebarData.js
@@ -10,6 +10,6 @@ export const SidebarDataBaker = [
 export const SidebarDataClient = [
   { title: "CAKER 소개", path: "/about" },
   { title: "전체 제안서 리스트", path: "/allproposal" },
-  { title: "제안서 작성하기", path: "/proposal" },
+  { title: "제안서 작성하기", path: "/create/city" },
   { title: "마이페이지", path: "/client/mypage" },
 ];
