26ce51ca0b108b37648a9dc5a98dc1d660078f2e
sohyundoh <lyny123@naver.com>
2022-08-05 18:23:38 +0200

    feat: select box 수정

--- src/components/WholeProposals/ChooseBox.js
+++ src/components/WholeProposals/ChooseBox.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import DropdownArrow1 from "../../images/DropdownArrow1.svg";
 import axios from "axios";
 const ContentBoxWithMargin = styled.div`
-  width: 381px;
+  width: 100%;
   height: 31px;
   margin-top: 50px;
   margin-bottom: 20px;
@@ -36,9 +36,12 @@ const ChoooseDisplay = styled.div`
   border-radius: 25px;
   border: 2px solid var(--sub-darkgray);
   margin-left:40px;
+  display: flex;
+  flex-direction: row;
 `;
 const DropdownArrow = styled.span`
   margin-right: 3px;
+  margin-top: 8px;
   & > img {
     width: 12px;
     margin-bottom: 4px;
@@ -65,8 +68,9 @@ const Dropdown = ({ items }) => {
     </ChoooseDisplay>
   );
 };
-const Seodaemun =['홍은동'];
-const CakeType = ['레터링케이크'];
+const Gu = ['서대문구']
+const Seodaemun = ['동 전체','북아현동','신촌동','연희동','홍제1동','홍제2동','홍제3동','홍은1동', '홍은2동','남가좌1동','남가좌2동','북가좌1동','북가좌2동'];
+const CakeType = ['레터링 케이크','미니(도시락) 케이크','생화 케이크','플라워앙금 케이크','컵 케이크','기타 케이크'];
 const ChooseBox = () => {
 
 
@@ -74,6 +78,7 @@ const ChooseBox = () => {
     <div>
       <ContentBoxWithMargin>
         <DropdownGroup>
+          <Dropdown items={Gu}></Dropdown>
           <Dropdown items={Seodaemun}></Dropdown>
           <Dropdown items={CakeType}></Dropdown>
         </DropdownGroup>
